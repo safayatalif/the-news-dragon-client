25433cf7f5e5a8c351c417ff7bf875c36da45c36
safayatalif <safayatalif2021@gmail.com>
2023-04-30 11:33:13 +0600

    env file fixed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,20 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// console.log("environment" , import.meta.env.VITE_APIKEY)
 const firebaseConfig = {
-  apiKey: "AIzaSyDusL2U0JvsPt7Jr4juEhzhcVR0Ynz9bkA",
-  authDomain: "the-news-dragon-a7a41.firebaseapp.com",
-  projectId: "the-news-dragon-a7a41",
-  storageBucket: "the-news-dragon-a7a41.appspot.com",
-  messagingSenderId: "754709996753",
-  appId: "1:754709996753:web:c9004e245f8f24133d11f0"
-  // apiKey: import.meta.env.VITE_APIKEY,
-  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  // projectId: import.meta.env.VITE_PROJECTID,
-  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_APPID
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
