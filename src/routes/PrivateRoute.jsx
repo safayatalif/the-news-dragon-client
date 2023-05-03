b379906fb6526551f9d200c928d8569de953ab1e
safayatalif <safayatalif2021@gmail.com>
2023-05-04 00:47:54 +0600

    some update

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,17 +4,17 @@ import { AuthContext } from '../Providers/AuthProviders';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
 
-const PrivateRoute = ({children}) => {
-    const {user , loading} = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-    if(loading){
+    if (loading) {
         return <Spinner animation='border' variant='primary'></Spinner>
     }
 
-    if(user){
+    if (user) {
         return children;
     }
-    return <Navigate state={{from:location}} replace to='/login'></Navigate>
+    return <Navigate state={{ from: location }} replace to='/login'></Navigate>
 };
 
 export default PrivateRoute;
